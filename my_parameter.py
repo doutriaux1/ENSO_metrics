8803c183778db1a487ef2d07ac3f5a076eacd0bd
Ji-Woo Lee <lee1043@crunchy.llnl.gov>
2017-09-27 16:35:10 -0700

    adjust parameter file

--- my_parameter.py
+++ my_parameter.py
@@ -1,14 +1,15 @@
-# Directory path for models and observation
-modpath = "/work/cmip5/piControl/atm/mo/ts/cmip5.MODS.piControl.r1i1p1.mo.atm.Amon.ts.ver-1.latestX.xml"
+# Directory path for observation
 obspath = "/clim_obs/obs/ocn/mo/tos/UKMETOFFICE-HadISST-v1-1/130122_HadISST_sst.nc"
-modnames = "ACCESS1-0" #"ACCESS1-0 ACCESS1-3"
-experiment = "piControl"
+
+# Directory path for models
+modpath = "/work/cmip5/piControl/atm/mo/VAR/cmip5.MOD.historical.r1i1p1.mo.atm.Amon.VAR.ver-1.latestX.xml"
+modnames = "IPSL-CM5A-LR" #"ACCESS1-0 ACCESS1-3"
 
 # Variables
 variable = "ts"
 variableobs = "sst"
 
 # Output
-outpathdata = "/work/lee1043/cdat/pmp/enso/scripts_PMPparser/test"
-outpathjsons = "/work/lee1043/cdat/pmp/enso/scripts_PMPparser/test"
-outnamejson = "cmip5_enso_bellenger_ACCESS1-0_ACCESS1-3.json"
+outpathdata = "/work/lee1043/cdat/pmp/enso/test"
+outpathjsons = "/work/lee1043/cdat/pmp/enso/test"
+outnamejson = "test.json"
