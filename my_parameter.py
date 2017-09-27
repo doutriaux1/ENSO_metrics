92830e477978ca4f47668acf1fad98285de72fde
Ji-Woo Lee <lee1043@crunchy.llnl.gov>
2017-09-27 16:21:59 -0700

    Add example of commend line options and parameter file

---
+++ my_parameter.py
@@ -0,0 +1,14 @@
+# Directory path for models and observation
+modpath = "/work/cmip5/piControl/atm/mo/ts/cmip5.MODS.piControl.r1i1p1.mo.atm.Amon.ts.ver-1.latestX.xml"
+obspath = "/clim_obs/obs/ocn/mo/tos/UKMETOFFICE-HadISST-v1-1/130122_HadISST_sst.nc"
+modnames = "ACCESS1-0" #"ACCESS1-0 ACCESS1-3"
+experiment = "piControl"
+
+# Variables
+variable = "ts"
+variableobs = "sst"
+
+# Output
+outpathdata = "/work/lee1043/cdat/pmp/enso/scripts_PMPparser/test"
+outpathjsons = "/work/lee1043/cdat/pmp/enso/scripts_PMPparser/test"
+outnamejson = "cmip5_enso_bellenger_ACCESS1-0_ACCESS1-3.json"
---
+++ sample_how_to_run.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import logging
+LOG_LEVEL = logging.INFO
+logging.basicConfig(level=LOG_LEVEL)
+
+import subprocess
+
+### Commend could be either way:
+
+cmd = """./driver_enso_metrics.py \
+         --mp /work/cmip5/piControl/atm/mo/ts/cmip5.MODS.piControl.r1i1p1.mo.atm.Amon.ts.ver-1.latestX.xml \
+         --op /clim_obs/obs/ocn/mo/tos/UKMETOFFICE-HadISST-v1-1/130122_HadISST_sst.nc \
+         --mns ACCESS1-0 ACCESS1-3 \
+         --var ts \
+         --varobs sst \
+         --outpd /work/lee1043/cdat/pmp/enso/test \
+         --outpj /work/lee1043/cdat/pmp/enso/test \
+         --outnj cmip5_enso_bellenger_ACCESS1-0_ACCESS1-3.json """
+
+# or..
+
+cmd = """./driver_enso_metrics.py -p my_parameter.py"""
+
+subprocess.Popen(cmd, shell=True)
